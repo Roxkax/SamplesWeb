2d07a06cdfbd0f767eb66df1b90cb8d17182ffa2
Diogo Gonçalves <dgoncalves@agap2.pt>
2022-07-18 12:38:07 +0100

    Added basic sample api

---
+++ SamplesWebApi.Data/Models/SampleApiItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SamplesWebApi.Data.Models
+{
+    public class SampleApiItem
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ SamplesWebApi/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SamplesWebApi.Data.Models;
+
+namespace SamplesWebApi.Controllers
+{
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly ILogger<SampleController> _logger;
+
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/api/Sampler/GetSampleApiItems")]
+        public IEnumerable<SampleApiItem> Get(int numberOfItems)
+        {
+            return Enumerable.Range(1, numberOfItems).Select(i => new SampleApiItem { Id = Guid.NewGuid(), Name = DateTime.Now.AddDays(i).ToString("dddd") }).ToArray();
+        }
+    }
+}
